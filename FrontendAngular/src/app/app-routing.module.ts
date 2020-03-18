bb1fe40d8a7edc8f531daabf3ce1ebac4364e02a
aya.zaki <aya.zaki17390@gmail.com>
2020-03-18 00:14:06 -0500

    resolve conflict app-routing-modules

--- FrontendAngular/src/app/app-routing.module.ts
+++ FrontendAngular/src/app/app-routing.module.ts
@@ -8,12 +8,10 @@ import { ResetComponent } from './reset/reset.component';
 import { AddPetComponent } from './add-pet/add-pet.component';
 import { AddpostComponent } from './addpost/addpost.component'
 import { ViewpostsComponent } from './viewposts/viewposts.component'
-<<<<<<< HEAD
 import { UserpetsComponent } from './userpets/userpets.component'
-=======
 import { AppComponent } from './app.component';
 import { UpdatePrfileComponent } from './update-prfile/update-prfile.component';
->>>>>>> a77bd521a0b3fd8f2dd1b2769a7dc69f93b1c140
+
 
 const routes: Routes = [
   { path: 'register', component: RegisterComponent },
@@ -26,12 +24,8 @@ const routes: Routes = [
   { path: 'viewpost', component: ViewpostsComponent },
   { path: 'addPet', component:AddPetComponent},
   { path: 'posts', component: AddpostComponent },
-<<<<<<< HEAD
-  { path: 'userpets', component: UserpetsComponent }
-
-=======
-  { path: 'updateProfile', component:UpdatePrfileComponent},
->>>>>>> a77bd521a0b3fd8f2dd1b2769a7dc69f93b1c140
+  { path: 'userpets', component: UserpetsComponent },
+  { path: 'updateProfile', component:UpdatePrfileComponent}
 ];
 
 @NgModule({
