e85c39e8c4fd5a0b9d7735d826a11045ecec0c2b
ALi Ansari <alidk2013@gmail.com>
2020-06-30 11:37:54 -0500

    update dependencies

--- Backend_Node_Express/controllers/userControllers.js
+++ Backend_Node_Express/controllers/userControllers.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const nodeoutlook = require('nodejs-nodemailer-outlook');
 var generator = require('generate-password');
 
